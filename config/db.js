0412e0c8eac51391888cdde0271bd0e7100b8aae
Amitdhal007 <amit0162.be21@chitkara.edu.in>
2024-07-04 10:30:04 +0530

    Added MealMover Backend

---
+++ config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://heyamit0162:JzYtMVf3H9Lh8AOe@cluster0.gcfq4jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
